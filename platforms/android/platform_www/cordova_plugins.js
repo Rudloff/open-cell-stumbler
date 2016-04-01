5129b71a08e710b788cf997fcf752cc50f251fb1
Pierre Rudloff <contact@rudloff.pro>
2016-04-02 01:30:36 +0200

    Update Android platform

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,38 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/pro.rudloff.celllocation/www/celllocation.js",
+        "id": "pro.rudloff.celllocation.CellLocation",
+        "pluginId": "pro.rudloff.celllocation",
+        "clobbers": [
+            "cordova.plugins.cellLocation"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.background-mode/www/background-mode.js",
+        "id": "de.appplant.cordova.plugin.background-mode.BackgroundMode",
+        "pluginId": "de.appplant.cordova.plugin.background-mode",
+        "clobbers": [
+            "cordova.plugins.backgroundMode",
+            "plugin.backgroundMode"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "pluginId": "org.apache.cordova.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.geolocation": "0.3.10",
+    "pro.rudloff.celllocation": "1.0.1",
+    "de.appplant.cordova.plugin.background-mode": "0.6.0-dev",
+    "org.apache.cordova.device": "0.2.13-dev"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/android/platform_www/plugins/pro.rudloff.celllocation/www/celllocation.js
@@ -0,0 +1,21 @@
+cordova.define("pro.rudloff.celllocation.CellLocation", function(require, exports, module) { var CellLocationLoader = function (require, exports, module) {
+    var exec = require("cordova/exec");
+    
+    function CellLocation () {}
+        
+    CellLocation.prototype.get = function (successFunc, failFunc) {
+        exec(successFunc, failFunc, "CellLocation","get",[]);
+    };
+    
+    var cellLocation = new CellLocation();
+    module.exports = cellLocation;
+};
+
+CellLocationLoader(require, exports, module);
+
+cordova.define("cordova/plugin/CellLocation", CellLocationLoader);
+
+
+
+
+});
